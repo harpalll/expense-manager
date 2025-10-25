1d04759dc16563f17427f29df2b16bb2bce779b7
Harpalsinh Sindhav <88263558+harpalll@users.noreply.github.com>
2025-10-26 00:32:12 +0530

    Feat: Changed Dates to Indian Style

--- src/features/expense/index.js
+++ src/features/expense/index.js
@@ -36,7 +36,9 @@ const formatDate = (expenseDate) => {
   const year = date.getFullYear();
   const month = String(date.getMonth() + 1).padStart(2, "0");
   const day = String(date.getDate()).padStart(2, "0");
-  return `${year}-${month}-${day}`;
+  // return `${year}-${month}-${day}`;
+  // ? indian style
+  return `${day}-${month}-${year}`;
 };
 
 // * table columns definition
